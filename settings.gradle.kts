a23ed6dd78c401dc95a48c5e248b927fa2977d09
Lyamin Tikhon <misterkukuruza784@gmail.com>
2024-08-30 19:15:27 +0300

    сreating basic empty modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,16 @@
 rootProject.name = "BaseProjectSetUp"
+
 include(":app")
+include(":data:home-data")
+include(":domain:home-domain")
+include(":base:ui-kit")
+include(":base:kotlin-utils")
+include(":feature:home-feature")
+include(":core:navigation-core")
+include(":core:database-core")
+include(":core:network-core")
+include(":core:preferences-core")
+include(":common:navigation-common")
 
 pluginManagement {
     includeBuild("build-logic")
