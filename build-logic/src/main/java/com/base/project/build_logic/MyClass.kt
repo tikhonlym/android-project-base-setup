e04609273b6021e13cddbb44a1bdb7107eaa410d
Lyamin Tikhon <misterkukuruza784@gmail.com>
2024-08-30 13:49:22 +0300

    Initial commit

---
+++ build-logic/src/main/java/com/base/project/build_logic/MyClass.kt
@@ -0,0 +1,4 @@
+package com.base.project.build_logic
+
+class MyClass {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "BaseProjectSetUp"
+include(":app")
+
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
